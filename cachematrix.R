01eb97886fb5f17f3f1c44500f8c00bb5e3d335f
Kheng <goh.yongkheng@gmail.com>
2015-05-17 23:06:32 +0800

    Necessary changes for Coursera R Programming course assignment 2.

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        s <- NULL
+        set <- function(y) {
+                x <<- y
+                s <<- NULL
+        }
+        get <- function() x
+        setsol <- function(solve) s <<- solve
+        getsol <- function() s
+        list(set = set, get = get,
+             setsol = setsol,
+             getsol = getsol)
 }
 
 
@@ -12,4 +22,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        s <- x$getsol()
+        if(!is.null(s)) {
+                message("getting cached data")
+                return(s)
+        }
+        data <- x$get()
+        s <- solve(data, ...)
+        x$setsol(s)
+        s
 }
